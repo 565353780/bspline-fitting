2d71b837f823a1c65d4b07fc2ca7b0498826e956
chLi <565353780@qq.com>
2024-06-24 22:42:24 +0800

    remove all archive python code

--- server.py
+++
@@ -1,4 +0,0 @@
-from bspline_fitting.Demo.server import demo as demo_server
-
-if __name__ == "__main__":
-    demo_server()
--- train.py
+++
@@ -1,4 +0,0 @@
-from bspline_fitting.Demo.trainer import demo as demo_train
-
-if __name__ == "__main__":
-    demo_train()
